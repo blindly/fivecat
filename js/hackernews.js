6083cc6c4cee32971c344dee25b975299de76c7e
blindly <wildkarde@elitemail.org>
2015-08-29 23:22:08 -0400

    Update hackernews.js

--- js/hackernews.js
+++ js/hackernews.js
@@ -3,11 +3,7 @@ $( document ).ready()
   /* News */
   var newsAPI = "http://www.reddit.com/r/hackernews/hot.json";
 
-  console.log('hello world');
-
   $.getJSON(newsAPI, function( json ){
-
-      console.log(json);
 
       var children = json.data.children;
 
@@ -15,10 +11,7 @@ $( document ).ready()
 
       $.each( children, function( key, value) {
         var title = children[key]['data']['title'];
-        var link = children[key]['data']['permalink'];
-
-        console.log(title);
-
+        var link = children[key]['data']['url'];
         var article_layout = "<div class='article'><a class='title' href='"+ link +"'>"+ title +"</a></div>";
 
         $('#news').append( article_layout );
