a826fb75f19df3773271023ecb2090c32c48415d
blindly <@blindly>
2015-10-05 21:16:00 -0400

    merging

--- js/news.js
+++ js/news.js
@@ -8,9 +8,14 @@ $(document).ready() {
 
             var title = stories[key]['story_title'];
             var link = stories[key]['story_permalink'];
-            var domain = url_domain(link);
-            var favicon = url_favicon(domain);
-            var article = article_layout(link, title, favicon);
+
+            var a = document.createElement('a');
+            a.href = link;
+            var domain = a.hostname;
+
+            var favicon = "<img src='http://www.google.com/s2/favicons?domain=" + domain + "'/>";
+
+            var article = "<div class='article'>" + favicon + "<a class='title' target='_blank' href='" + link + "'>" + title + "</a></div>";
 
             $('#news').append(article);
         });
