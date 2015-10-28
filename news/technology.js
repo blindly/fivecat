7a69cee7d67ef06264cde55e6edf7d64f53440b9
blindly <wildkarde@elitemail.org>
2015-10-28 21:05:45 +0100

    adding gitlab

--- news/technology.js
+++ news/technology.js
@@ -7,8 +7,8 @@
             'http://blogs.opera.com/desktop/feed',
             'http://feeds.feedburner.com/PuppetLabs',
             'http://blog.docker.com/feed/',
+            'https://about.gitlab.com/atom.xml'
             'http://www.vmwareblogs.com/rss.xml?flag=2'];
-
         feeds.forEach(function (entry) {
             var feed = new google.feeds.Feed(entry);
             feed.load(function (result) {
