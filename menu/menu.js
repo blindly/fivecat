253e33341530f6d506e5fa94c2ce6c0ff844a618
blindly <@blindly>
2015-10-31 17:46:20 +0000

    adding js based menu

---
+++ menu/menu.js
@@ -0,0 +1,46 @@
+(function(){
+
+    // All items we'd like to add
+    var navItems = [
+        {href: 'index.html', text: 'Recent'},
+        {href: 'hn.html', text: 'Hacker News'},
+        {href: 'business.html', text: 'Business'},
+        {href: 'world.html', text: 'World'},
+        {href: 'technology.html', text: 'Tech'},
+        {href: 'internet.html', text: 'Internet'}
+    ];
+
+    // A few variables for use later
+    var navElem = document.createElement("nav"),
+        navList = document.createElement("ul"),
+        navItem, navLink;
+
+    //var navElem =      document.getElementById("nav");
+
+    navElem.appendChild(navList);
+
+    // Cycle over each nav item
+    for (var i = 0; i < navItems.length; i++) {
+        // Create a fresh list item, and anchor
+        navItem = document.createElement("li");
+        navLink = document.createElement("a");
+
+        // Set properties on anchor
+        navLink.href = navItems[i].href;
+        navLink.innerHTML = navItems[i].text;
+
+        // Add anchor to list item, and list item to list
+        navItem.appendChild(navLink);
+        navList.appendChild(navItem);
+    }
+
+    // Set first list item as current
+    navList.children[0].className = "active";
+
+    // Add list to body (or anywhere else)
+    window.onload = function () {
+        //document.body.appendChild(navElem);
+        document.getElementById("nav").appendChild(navElem);
+    }
+
+}());
