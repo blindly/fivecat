5a1340c755bceefd67fed4a9259f17be93caa18a
blindly <@blindly>
2015-11-02 10:38:06 +0000

    Using new source for cybersecurity

--- news/security.js
+++ news/security.js
@@ -1,32 +1,24 @@
-    google.load("feeds", "1");
+document.addEventListener('DOMContentLoaded', function () {
+    var newsAPI = "https://www.reddit.com/r/netsec/hot.json";
 
-    function initialize() {
-        var feeds = [
-                    'http://feeds.feedburner.com/co/TbcL',
-                    'http://feeds.feedburner.com/homelandsecuritynewswire/rss',
-                    'http://www.huffingtonpost.com/tag/cybersecurity/feed/'
-            ];
-        feeds.forEach(function (entry) {
-            var feed = new google.feeds.Feed(entry);
-            feed.load(function (result) {
-                if (!result.error) {
-                    for (var num = 0; num < result.feed.entries.length; num++) {
-                        var entry = result.feed.entries[num];
-                        var title = entry.title;
-                        var link = entry.link;
-                        var a = document.createElement('a');
-                        a.href = link;
-                        var domain = a.hostname;
+    $.getJSON(newsAPI, function (json) {
 
-                        var favicon = "<img style='width: 20px !important; margin-bottom: 0 !important; padding-right: 10px' src='http://www.google.com/s2/favicons?domain=" + domain + "'/>";
+        var children = json.data.children;
 
-                        var article = "<div class='article'><a class='title' target='_blank' href='" + link + "'>" + favicon + title + "</a></div>";
+        console.log(children);
 
-                        $('#news').append(article);
-                    }
-                }
-            });
+        $.each(children, function (key, value) {
+            var title = children[key]['data']['title'];
+            var link = children[key]['data']['url'];
+            var a = document.createElement('a');
+            a.href = link;
+            var domain = a.hostname;
+
+            var favicon = "<img style='width: 20px !important; margin-bottom: 0 !important; padding-right: 10px' src='http://www.google.com/s2/favicons?domain=" + domain + "'/>";
+
+            var article = "<div class='article'><a class='title' target='_blank' href='" + link + "'>" + favicon + title + "</a></div>";
+
+            $('#news').append(article);
         });
-    }
-
-    google.setOnLoadCallback(initialize);
+    });
+}, false);
