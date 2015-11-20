2f9202eb6b1d532c0f6565b826cdd987c23d76a4
blindly <@blindly>
2015-11-20 22:53:33 +0100

    adding instapaper

--- news/technology.js
+++ news/technology.js
@@ -24,6 +24,9 @@
                         a.href = link;
                         var domain = a.hostname;
 
+                        var instapaper = 'https://www.instapaper.com/text?u=';
+                        link = instapaper + link;
+
                         var favicon = "<img style='width: 20px !important; margin-bottom: 0 !important; padding-right: 10px' src='http://www.google.com/s2/favicons?domain=" + domain + "'/>";
 
                         var article = "<div class='article'><a class='title' target='_blank' href='" + link + "'>" + favicon + title + "</a></div>";
