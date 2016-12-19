14e5c157adfa90061c684597a5b94471ab3d1376
blindly <blindly@noreply.github.com>
2016-12-19 11:37:07 -0500

    adding weekly roundup of news

--- public/assets/js/menu/menu.js
+++ public/assets/js/menu/menu.js
@@ -11,6 +11,7 @@
         {href: '/read/gaming', text: 'gaming'},
         //{href: 'it.html', text: 'it'},
         {href: '/read/learn', text: 'learn'},
+        {href: '/read/week', text: 'weekly roundup'},
         // {href: 'technology.html', text: 'tech'},
         {href: '/read/data', text: 'interesting'},
         {href: '/read/life', text: 'life advice'},
