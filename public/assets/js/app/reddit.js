27b9b212aec80a11e0d801df01378415e1046f84
blindly <blindly@noreply.github.com>
2016-12-19 11:42:43 -0500

    adding weekly roundup of news

--- public/assets/js/app/reddit.js
+++ public/assets/js/app/reddit.js
@@ -1,11 +1,14 @@
-function reddit(url) {
+function reddit(url, instapaper = false) {
   document.addEventListener('DOMContentLoaded', function () {
     $.getJSON(url, function (json) {
       var children = json.data.children;
       $.each(children, function (key, value) {
         var title = children[key]['data']['title'];
         var link = children[key]['data']['url'];
-        //link = instapaperIt(link);
+
+        if ( instapaper ) 
+          link = instapaperIt(link);
+          
         var favicon_img = generateFavicon(link);
         var favicon = "<img style='width: 20px !important; margin-bottom: 0 !important; padding-right: 10px' src='" + favicon_img + "'/>";
         var article = "<article><p><a class='title' target='_blank' href='" + link + "'>" + favicon + title + "</a></p></article>";
