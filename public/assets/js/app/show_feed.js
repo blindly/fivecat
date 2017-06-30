293d8c0c984f646e2f5c56a0e86c858305e9a624
blindly <blindly@users.noreply.github.com>
2017-06-30 11:26:41 -0400

    rel=noopener

--- public/assets/js/app/show_feed.js
+++ public/assets/js/app/show_feed.js
@@ -8,7 +8,7 @@ function show_feed(data) {
       let favicon_img = stories[key]['favicon'];
 
       let favicon = "<img style='width: 20px !important; margin-bottom: 0 !important; padding-right: 10px' src='" + favicon_img + "'/>";
-      let article = "<article><a class='title' target='_blank' href='" + link + "'>" + favicon + title + "</a></article>";
+      let article = "<article><a class='title' target='_blank' rel='noopener' href='" + link + "'>" + favicon + title + "</a></article>";
 
       const isPresent = bucket.includes(title);
 
